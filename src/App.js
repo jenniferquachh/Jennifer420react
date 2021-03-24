3cc5603a1babf311d8be4b57244d421cd8fa6fe1
Jennifer Quach <jnnifrquach@gmail.com>
2021-03-24 15:06:12 -0400

    installed react router

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import {
   Route,
   Link
 } from 'react-router-dom';
-import logo from './logo.svg';
+import Home from './pages/Home.js';
+import logo from './images/logo.svg';
 import './App.css';
 
 export default function App() {
@@ -51,14 +52,7 @@ export default function App() {
           </Route>
 
           <Route path="/">
-            {/* <Home />*/}
-            <div className="App">
-              <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p> HOME <code>src/App.js</code> and save to reload.</p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
-              </header>
-            </div>
+            <Home />
           </Route>
         </Switch>
       </div>
